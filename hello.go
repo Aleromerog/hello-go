d4129f8adc4004a91f6ea9439dfc6169c781ee8a
Alejandro Romero <aleromero@google.com>
2023-04-18 19:57:32 +0000

    add test

--- hello.go
+++ hello.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"errors"
+	"log"
+
 	"fmt"
 )
 
 func main() {
 	// Get a greeting message and print it.
-	message := Hello("Catalog")
+	message, err := Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
 
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
